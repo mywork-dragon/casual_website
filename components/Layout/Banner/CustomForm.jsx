b7f2e1d57c8af5108d827d9b372d904db5336447
ihassansattar <i.hassansattar@gmail.com>
2023-06-09 21:17:27 +0500

    banner mailchimp form integration

---
+++ components/Layout/Banner/CustomForm.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+
+const CustomForm = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+      email.indexOf("@") > -1 &&
+      onValidated({
+        EMAIL: email,
+      });
+  };
+  const clearFields = () => {
+    setEmail("");
+  };
+  useEffect(() => {
+    if (status === "success") clearFields();
+  }, [status]);
+  return (
+    <div className="w-full mt-4 lg:mt-8">
+      {status === "error" && (
+        <div
+          className="w-full mb-2 text-red-600 text-base leading-4 font-bold"
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      {status === "success" ? (
+        <div
+          className="w-full text-text-300 font-extrabold text-lg leading-6 text-center"
+          dangerouslySetInnerHTML={{
+            __html: "You’re in! Thank you for signing up",
+          }}
+        />
+      ) : (
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          target="_self"
+          className="w-full sm:w-4/5 md:w-full h-12 sm:h-9 md:h-10 lg:h-[50px] bg-white p-0.5 pl-4 sm:pl-8 rounded-[53px] shadow-input flex items-center justify-between validate"
+        >
+          <div className="w-full mr-4">
+            <input
+              className="bg-transparent w-full font-lora font-normal text-15 leading-6 required email"
+              placeholder="Your email goes here"
+              required
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              htmlFor="email"
+              name="EMAIL"
+              id="mce-EMAIL"
+            />
+          </div>
+          <button
+            type="submit"
+            className="h-full px-6 uppercase hover:opacity-80 transition-all duration-300 text-white bg-text-300 tracking-wide font-bold text-xs leading-4 rounded-[53px]"
+          >
+            Subscribe
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default CustomForm;
--- components/Layout/Banner/index.jsx
+++ components/Layout/Banner/index.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import CustomForm from "./CustomForm";
 
 const Banner = () => {
-  const [email, setEmail] = useState("");
+  const url =
+    "https://causallabs.us21.list-manage.com/subscribe/post?u=90fb20275d7f65ea298fd2a50&amp;id=3dfa4b7f7e&amp;f_id=00205ae1f0";
   return (
     <div className="w-full sm:mt-4 lg:mt-24 relative top-16">
       <div className="w-full max-w-7xl px-3 sm:px-6 md:px-9 xl:px-6 mx-auto">
@@ -15,34 +18,18 @@ const Banner = () => {
               Get the latest tips on how to build, ship and optimize your
               consumer tech products, delivered straight to your inbox.
             </p>
-            <form
-              action="https://causallabs.us21.list-manage.com/subscribe/post?u=90fb20275d7f65ea298fd2a50&amp;id=3dfa4b7f7e&amp;f_id=00205ae1f0"
-              method="post"
-              id="mc-embedded-subscribe-form"
-              name="mc-embedded-subscribe-form"
-              target="_self"
-              className="w-full sm:w-4/5 md:w-full h-12 sm:h-9 md:h-10 lg:h-[50px] bg-white p-0.5 pl-4 sm:pl-8 mt-4 lg:mt-8 rounded-[53px] shadow-input flex items-center justify-between validate"
-            >
-              <div className="w-full mr-4">
-                <input
-                  className="bg-transparent w-full font-lora font-normal text-15 leading-6 required email"
-                  placeholder="Your email goes here"
-                  required
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  htmlFor="email"
-                  name="EMAIL"
-                  id="mce-EMAIL"
-                />
-              </div>
-              <button
-                type="submit"
-                className="h-full px-6 uppercase hover:opacity-80 transition-all duration-300 text-white bg-text-300 tracking-wide font-bold text-xs leading-4 rounded-[53px]"
-              >
-                Subscribe
-              </button>
-            </form>
+            <div className="w-full">
+              <MailchimpSubscribe
+                url={url}
+                render={({ subscribe, status, message }) => (
+                  <CustomForm
+                    status={status}
+                    message={message}
+                    onValidated={(formData) => subscribe(formData)}
+                  />
+                )}
+              />
+            </div>
           </div>
         </div>
       </div>
