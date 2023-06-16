0611e39a56cc9494288a7401c5a5cb466098514e
ihassansattar <i.hassansattar@gmail.com>
2023-06-16 21:43:43 +0500

    Testimonials update

--- components/Landing/Testimonial/index.jsx
+++ components/Landing/Testimonial/index.jsx
@@ -9,24 +9,24 @@ const Testimonial = () => {
           style={{ backgroundImage: "url(/images/quote.png)" }}
           className="w-full bg-no-repeat bg-center bg-contain flex flex-col justify-center items-center"
         >
-          <div className="rounded-full">
-            <img
-              src="/images/TonyDeigh.png"
-              className="h-auto object-cover w-20 rounded-full"
-            />
-          </div>
-          <div className="mt-2 sm:mt-4 max-w-3.5xl">
+          <div className="mt-2 sm:mt-4 max-w-3.5xl flex items-center flex-col">
             <p className="font-lora text-text-100 text-base leading-6 sm:text-lg sm:leading-6.5 text-center font-normal">
               “We had 4 A/B testing platforms—none of which met our needs—and
               I’ve been trying for years to unify them and solve our problems
               across our stack. We’ve started eliminating those other platforms
               and Causal has increased the velocity of our experimentation.”
             </p>
-            <p className="text-lg leading-6.5 text-text-300 text-center mt-4 underline-offset-[2px] decoration-[#99F876] underline font-bold">
-              <TextGraphic height="12px" bottom="1px">
-                Tony Deigh
-              </TextGraphic>
-            </p>
+            <div className="rounded-full mt-4 flex items-center">
+              <img
+                src="/images/TonyDeigh.png"
+                className="h-auto object-cover w-20 rounded-full"
+              />
+              <p className="text-lg mb-2.5 leading-6.5 text-text-300 text-center underline-offset-[2px] decoration-[#99F876] underline font-bold">
+                <TextGraphic height="12px" bottom="1px">
+                  Tony Deigh
+                </TextGraphic>
+              </p>
+            </div>
             <p className="text-lg leading-6.5 text-text-300 text-center font-bold">
               Co-Founder & Chief Data Officer, JobCase
             </p>
